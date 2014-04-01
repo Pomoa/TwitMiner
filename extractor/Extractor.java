3d6ecb86406d081d56a29b4225bef3530b20634c
Stilldead <pierreburgain@gmail.com>
2014-04-01 18:08:27 +0200

    ecriture dans le fichier CSV des tweets

--- extractor/Extractor.java
+++ extractor/Extractor.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.StringWriter;
 import java.util.*;
 
 import twitter4j.conf.*; 
@@ -8,7 +15,7 @@ import twitter4j.api.*;
 import twitter4j.util.*; 
 import twitter4j.*;
 
-	
+
 public class Extractor {
 
 	static TwitterFactory twitterFactory; 
@@ -29,32 +36,51 @@ public class Extractor {
 		cb.setOAuthAccessTokenSecret("5RIn2tWgFSRk1w86MTHsnDxgGSkezt50iHbkBxJp10eNr");   
 		twitterFactory = new TwitterFactory(cb.build());   
 		twitter = twitterFactory.getInstance();  
-	}  
+	}
+	
+	public static void WritterCSV (ArrayList <Status> tweets) throws FileNotFoundException {
+		File csvFile = new File("exemple.csv"); 
+		if (!csvFile.exists()) 
+			throw new FileNotFoundException("Le fichier n'existe pas"); 
+		else{
+			PrintStream l_out = new PrintStream(new FileOutputStream("exemple.csv", true)); 
+		
+		l_out.print(tweets);  
+		l_out.flush(); 
+		l_out.close(); 
+		l_out=null;
+		
+		}
+	}
 
-	static void draw() {   
+	static void draw() throws FileNotFoundException {   
 		try {     
 			requete = new Query("#ThanksTokioHotelForTheNewSong");     
 			requete.setCount(100);
 			resultat = twitter.search(requete);
-
-			tweets = (ArrayList<Status>) resultat.getTweets();
-			for (int i = 0; i < tweets.size(); i++) {       
-				Status t=(Status) tweets.get(i);       
-				User u=(User) t.getUser();       
-				user=u.getName();      
-				pseudo = t.getUser().getScreenName();       
-				Date date = t.getCreatedAt();
+			
+			
+			for ( int p =0; p < 100; p++ )
+			{
+				tweets = (ArrayList<Status>) resultat.getTweets();
+				for (int i = 0; i < tweets.size(); i++) {       
+					Status t=(Status) tweets.get(i);       
+					User u=(User) t.getUser();       
+					user=u.getName();      
+					pseudo = t.getUser().getScreenName();       
+					Date date = t.getCreatedAt();
+				}
+				WritterCSV(tweets);
 			}
+			
+			
 			System.out.println(tweets.size());
 		} catch (TwitterException e) {     
 				System.out.println("Couldn't connect: " + e);   
 		} 
 	}
-			
-		
-	
-	
-	public static void main (String [] arg){
+
+	public static void main (String [] arg) throws FileNotFoundException{
 		setup();
 		draw();
 	}
