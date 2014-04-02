3268b7530e6b60b97b300955b8dc65903e88d84d
Pomoa <tristan.ughetti@pomoa.fr>
2014-04-02 16:49:35 +0200

    P0 fonctionnel
    Toutes les fonctions de P0 sont réalisées

--- extractor/Extractor.java
+++ extractor/Extractor.java
@@ -23,7 +23,7 @@ public class Extractor {
 	RequestToken requestToken; 
 	static Query requete; 
 	static QueryResult resultat;  
-	static ArrayList<Status> tweets;  
+	static ArrayList<Status> tweets = new ArrayList();  
 	static String msg;
 	static String user;
 	static String pseudo;
@@ -41,46 +41,54 @@ public class Extractor {
 	public static void WritterCSV (ArrayList <Status> tweets) throws FileNotFoundException {
 		File csvFile = new File("exemple.csv");
 		try {
+			csvFile.delete();
 			csvFile.createNewFile();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		if (!csvFile.exists()) 
 			throw new FileNotFoundException("Le fichier n'existe pas"); 
-		else{
-			PrintStream l_out = new PrintStream(new FileOutputStream("exemple.csv", true)); 
-		
-		l_out.print(tweets);  
-		l_out.flush(); 
-		l_out.close(); 
-		l_out=null;
-		
+		else {
+			PrintStream l_out = new PrintStream(new FileOutputStream("exemple.csv", true));
+
+			for (int i = 0; i < tweets.size(); i++) { 
+				Status t = tweets.get(i);
+				l_out.print("\"" + t.getCreatedAt() + "\";\"" + '@' + t.getUser().getScreenName() + "\";\"");
+				for (int j = 0; j < t.getText().length() ; ++j) {
+					if(!Character.isWhitespace(t.getText().charAt(j))) {
+						l_out.print(t.getText().charAt(j));
+					} else {
+						l_out.print("\";\"");
+					}
+				}
+				l_out.println('"');
+			}
+			l_out.flush(); 
+			l_out.close(); 
+			l_out=null;
+			
 		}
 	}
 
 	static void draw() throws FileNotFoundException {   
 		try {     
-			requete = new Query("#ThanksTokioHotelForTheNewSong");
+			requete = new Query("#GouvernementValls");
 			
 			
-			for ( int p =0; p < 100; p++ )
-			{     
+			for ( int p =0 ; p < 100 ; p++ )
+			{
 				requete.setCount(100);
 				resultat = twitter.search(requete);
-				tweets = (ArrayList<Status>) resultat.getTweets();
-				for (int i = 0; i < tweets.size(); i++) {       
-					Status t=(Status) tweets.get(i);       
-					User u=(User) t.getUser();       
-					user=u.getName();      
-					pseudo = t.getUser().getScreenName();       
-					Date date = t.getCreatedAt();
-				}
+				tweets.addAll(resultat.getTweets());
 				WritterCSV(tweets);
+				requete.setMaxId(tweets.get(tweets.size()-1).getId()-1);
+				
+				System.out.println("" + p + '%');
 			}
 		} catch (TwitterException e) {     
 				System.out.println("Couldn't connect: " + e);   
-		} 
+		}
+		System.out.print("Nombre de tweets : " + tweets.size());
 	}
 
 	public static void main (String [] arg) throws FileNotFoundException{
