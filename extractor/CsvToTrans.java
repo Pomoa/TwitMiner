dfd509c584dad4262e93e6226b92f8386aa4fdfa
Stilldead <pierreburgain@gmail.com>
2014-04-05 23:28:47 +0200

    lol

--- extractor/CsvToTrans.java
+++ extractor/CsvToTrans.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +10,26 @@ import java.io.PrintStream;
 
 
 public class CsvToTrans {
+	
+	public static boolean existeDeja(String mot) throws IOException {
+		InputStream ips2=new FileInputStream("fichierTraduit.txt");
+		InputStreamReader ipsr2=new InputStreamReader(ips2);
+		BufferedReader br2=new BufferedReader(ipsr2);
+		String test = br2.readLine();
+		for (int i = 0 ; i < test.length(); i++) {
+			if ( test.charAt(i) == '=')
+			{
+				for (int p = 0 ; mot.length() > p ; p++, i++) {
+					if ( i == test.length()) break;
+					if ( test.charAt(i) != mot.charAt(p) )
+						return false;
+				}
+				return true;
+				
+			}
+		}
+		return true;
+	}
 	
 	public static void csvToTrans() throws IOException {
 		int cpt = 0;
@@ -44,34 +65,39 @@ public class CsvToTrans {
 					}
 				} else {
 					String mot = new String();
+//					BufferedReader br2=new BufferedReader(ipsr2);
+//					String test = br2.readLine();
 					for ( ; ligne.charAt(i) !='"'; ) {
 						mot += ligne.charAt(i++);
-						System.out.println(mot);
+						//System.out.println(mot);
 						if(i >= ligne.length()) break;
 					}
-					System.out.println(mot);
+					//System.out.println(mot);
+					
+					//System.out.println(mot);
 					if ( cpt == 0) {
 						cpt++;
 						l_out.println("" + cpt + "=" + mot);
 					} else {
-						BufferedReader br2=new BufferedReader(ipsr2);
-						String test = br2.readLine();
-						System.out.println(mot);
-						for ( ; test !=null ; ) {
-							System.out.println(mot);
-							if (!test.contains(mot)) {
-								//System.out.println(test);
-								cpt++;
-								l_out.println("" + cpt + "=" + mot);
-								break;
-								
-							}
-						test = br2.readLine();
-						}
 						
+						if (!existeDeja(mot)) {
+							System.out.println(mot);
+							l_out.println("" + cpt + "=" + mot);
+						}
+//						System.out.println(test);
+//						for ( ; test !=null ; ) {
+//							System.out.println(test);
+//							if (!test.contains(mot)) {
+//								System.out.println(test);
+//								cpt++;
+//								l_out.println("" + cpt + "=" + mot);
+//							}
+//						test = br2.readLine();
+//						}
+//						System.out.println(test);
 					}
 				}
-			
+				
 			}
 			ligne = br.readLine();
 		}
